0d1ae26a5acb099d061b453826f1b53203dcfc12
Kosal Nim <kosall123nim@gmail.com>
2024-09-25 20:56:36 +0700

    Create BrandServiceimpl.java

--- src/main/java/com/kosal/modernPhoneshop/mobile_phone/service/impl/BrandServiceImpl.java
+++ src/main/java/com/kosal/modernPhoneshop/mobile_phone/service/impl/BrandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kosal.modernPhoneshop.mobile_phone.service.impl;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +11,23 @@ import org.springframework.stereotype.Service;
 import com.kosal.modernPhoneshop.mobile_phone.brandSpec.BrandFilter;
 import com.kosal.modernPhoneshop.mobile_phone.brandSpec.BrandSpecification;
 import com.kosal.modernPhoneshop.mobile_phone.entities.Brand;
+import com.kosal.modernPhoneshop.mobile_phone.entities.Model;
 import com.kosal.modernPhoneshop.mobile_phone.exception.ResourceNotFoundException;
 import com.kosal.modernPhoneshop.mobile_phone.repository.BrandRepository;
+import com.kosal.modernPhoneshop.mobile_phone.repository.ModelRepository;
 import com.kosal.modernPhoneshop.mobile_phone.service.BrandService;
 import com.kosal.modernPhoneshop.mobile_phone.service.util.PageUtility;
 
 import lombok.RequiredArgsConstructor;
+
 @Service
 @RequiredArgsConstructor
-public class BrandServiceImpl implements BrandService{
+public class BrandServiceImpl implements BrandService {
 	@Autowired
 	private final BrandRepository brandRepository;
+	@Autowired
+	private final ModelRepository modelRepository;
+	
 
 	@Override
 	public Brand create(Brand brand) {
@@ -29,98 +36,109 @@ public class BrandServiceImpl implements BrandService{
 	}
 
 	@Override
-	public Brand getById(Integer id) {
-		/*Optional<Brand> optional = brandRepository.findById(id);*/
-		/*if(optional.isPresent()) {
-			return optional.get();
-			
-		}
-		
-			//throw new HttpClientErrorException(HttpStatus.NOT_FOUND,"Brand with id= "+id+"Not found!!");
-		//throw new HttpClientErrorException(HttpStatus.NOT_FOUND,"Brand with id=%dNot found!!".formatted(id));
-		*/
+	public Brand getById(Long id) {
+		/* Optional<Brand> optional = brandRepository.findById(id); */
+		/*
+		 * if(optional.isPresent()) { return optional.get();
+		 * 
+		 * }
+		 * 
+		 * //throw new HttpClientErrorException(HttpStatus.NOT_FOUND,"Brand with id= "
+		 * +id+"Not found!!"); //throw new
+		 * HttpClientErrorException(HttpStatus.NOT_FOUND,"Brand with id=%dNot found!!"
+		 * .formatted(id));
+		 */
 		return brandRepository.findById(id)
-				//.orElseThrow(()->new HttpClientErrorException(HttpStatus.NOT_FOUND,"Brand with id=%d Not found!!".formatted(id)));
-				.orElseThrow(()->new ResourceNotFoundException("Brand", id));
+				// .orElseThrow(()->new HttpClientErrorException(HttpStatus.NOT_FOUND,"Brand
+				// with id=%d Not found!!".formatted(id)));
+				.orElseThrow(() -> new ResourceNotFoundException("Brand",id));
 	}
 
 	@Override
-	public Brand update(Integer id, Brand uptodate) {
+	public Brand update(Long id, Brand uptodate) {
 		Brand brand = getById(id);
-		brand.setName(uptodate.getName());//to Improve
-   		return brandRepository.save(brand);
+		brand.setName(uptodate.getName());// to Improve
+		return brandRepository.save(brand);
 	}
 
 	@Override
-	public void delete(Integer id) {
+	public void remove(Long id) {
 		Brand brand = getById(id);
-		if(brand!=null) {
-			brandRepository.delete(brand);
-		}
-		
-		
+		brandRepository.delete(brand);
+
 	}
 
-	/*@Override
-	public List<Brand> getBrands(Map<String, String> params) {
-		BrandFilter filter =new BrandFilter();
-		if(params.containsKey("name")) {
-			String name = params.get("name");
-			filter.setName(name);
-		}
-		if(params.containsKey("id")) {
-			String id=params.get("id");
-			filter.setId(Integer.parseInt(id));
-		}
-		int pageLimit=1;
-		if(params.containsKey(PageUtility.PAGE_LIMIT)) {
-			pageLimit=Integer.parseInt(params.get(PageUtility.PAGE_LIMIT));
-			
-		}
-		int pageNumber=1;
-		if(params.containsKey(PageUtility.PAGE_NUMBER)) {
-			pageNumber=Integer.parseInt(params.get(PageUtility.PAGE_NUMBER));
-			
-		}
-		BrandSpecification specification =new BrandSpecification(filter);
-		Pageable pageable=PageUtility.getPageable(pageNumber, pageLimit);
-		return brandRepository.findAll(specification);
-	}*/
+	/*
+	 * @Override public List<Brand> getBrands(Map<String, String> params) {
+	 * BrandFilter filter =new BrandFilter(); if(params.containsKey("name")) {
+	 * String name = params.get("name"); filter.setName(name); }
+	 * if(params.containsKey("id")) { String id=params.get("id");
+	 * filter.setId(Integer.parseInt(id)); } int pageLimit=1;
+	 * if(params.containsKey(PageUtility.PAGE_LIMIT)) {
+	 * pageLimit=Integer.parseInt(params.get(PageUtility.PAGE_LIMIT));
+	 * 
+	 * } int pageNumber=1; if(params.containsKey(PageUtility.PAGE_NUMBER)) {
+	 * pageNumber=Integer.parseInt(params.get(PageUtility.PAGE_NUMBER));
+	 * 
+	 * } BrandSpecification specification =new BrandSpecification(filter); Pageable
+	 * pageable=PageUtility.getPageable(pageNumber, pageLimit); return
+	 * brandRepository.findAll(specification); }
+	 */
 	@Override
 	public Page<Brand> getBrands(Map<String, String> params) {
-		BrandFilter filter =new BrandFilter();
-		if(params.containsKey("name")) {
+		BrandFilter filter = new BrandFilter();
+		if (params.containsKey("name")) {
 			String name = params.get("name");
 			filter.setName(name);
 		}
-		if(params.containsKey("id")) {
-			String id=params.get("id");
-			filter.setId(Integer.parseInt(id));
+		if (params.containsKey("id")) {
+			String id = params.get("id");
+			filter.setId(Long.parseLong(id));
 		}
-		int pageLimit=1;
-		if(params.containsKey(PageUtility.PAGE_LIMIT)) {
-			pageLimit=Integer.parseInt(params.get(PageUtility.PAGE_LIMIT));
-			
+		int pageLimit = 1;
+		if (params.containsKey(PageUtility.PAGE_LIMIT)) {
+			pageLimit = Integer.parseInt(params.get(PageUtility.PAGE_LIMIT));
+
 		}
-		int pageNumber=1;
-		if(params.containsKey(PageUtility.PAGE_NUMBER)) {
-			pageNumber=Integer.parseInt(params.get(PageUtility.PAGE_NUMBER));
-			
+		int pageNumber = 1;
+		if (params.containsKey(PageUtility.PAGE_NUMBER)) {
+			pageNumber = Integer.parseInt(params.get(PageUtility.PAGE_NUMBER));
+
 		}
-		BrandSpecification specification =new BrandSpecification(filter);
-		Pageable pageable=PageUtility.getPageable(pageNumber, pageLimit);
-		 Page<Brand> findAll = brandRepository.findAll(pageable);
-		 return findAll;
+		BrandSpecification specification = new BrandSpecification(filter);
+		Pageable pageable = PageUtility.getPageable(pageNumber, pageLimit);
+		Page<Brand> findAll = brandRepository.findAll(pageable);
+		return findAll;
 	}
 
-
-	/*@Override
+	/*
+	 * @Override public List<Brand> getBrands() {
+	 * 
+	 * 
+	 * return brandRepository.findAll(); }
+	 * 
+	 */
+	@Override
 	public List<Brand> getBrands() {
 		// TODO Auto-generated method stub
 		return brandRepository.findAll();
 	}
 
 	@Override
+	public List<Model> getModelByBrandId(Long id) {
+		Brand brand = brandRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Brand", id));
+				brand=getById(id);
+		if(brand!=null) {
+			
+		if(brand.getId()==id) {
+				return modelRepository.findAll();
+			}
+		}
+		
+		return null;
+	}
+
+	/*@Override
 	public List<Brand> getBrands(String name) {
 		// TODO Auto-generated method stub
 		return brandRepository.findByNameContaining(name);
@@ -130,7 +148,6 @@ public class BrandServiceImpl implements BrandService{
 	public List<Brand> getBrand(String name) {
 		// TODO Auto-generated method stub
 		 return brandRepository.findByNameIgnoreCase("%" +name.toLowerCase() +"%");
-	}
-	*/
+	}*/
 
 }
