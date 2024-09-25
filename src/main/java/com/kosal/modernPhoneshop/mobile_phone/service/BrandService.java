ba0253a4d17364f421046cecea26191c0dcdbb54
Kosal Nim <kosall123nim@gmail.com>
2024-09-25 20:57:28 +0700

    Create BrandService.jav

--- src/main/java/com/kosal/modernPhoneshop/mobile_phone/service/BrandService.java
+++ src/main/java/com/kosal/modernPhoneshop/mobile_phone/service/BrandService.java
@@ -6,13 +6,15 @@ import java.util.Map;
 import org.springframework.data.domain.Page;
 
 import com.kosal.modernPhoneshop.mobile_phone.entities.Brand;
+import com.kosal.modernPhoneshop.mobile_phone.entities.Model;
 
 public interface BrandService {
 	Brand create(Brand brand);
-	Brand getById(Integer id);
-	Brand update(Integer id,Brand uptodate);
-	//List<Brand>getBrands();
-	void delete(Integer id);
+	Brand getById(Long id);
+	Brand update(Long id,Brand uptodate);
+	List<Brand>getBrands();
+	void remove(Long id);
+	List<Model>getModelByBrandId(Long id);
 	//List<Brand>getBrands(String name);
 	//List<Brand> getBrand(String name);
 	//List<Brand>getBrands(Map<String, String> params);
