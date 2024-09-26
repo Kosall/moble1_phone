c20fc6eefc6bc16032614866306e047994eba33b
Kosal Nim <kosall123nim@gmail.com>
2024-09-26 09:36:57 +0700

    Edit some in ModeEntityMappler.java

--- src/main/java/com/kosal/modernPhoneshop/mobile_phone/mapper/ModeEntityMapper.java
+++ src/main/java/com/kosal/modernPhoneshop/mobile_phone/mapper/ModeEntityMapper.java
@@ -6,13 +6,18 @@ import org.mapstruct.factory.Mappers;
 
 import com.kosal.modernPhoneshop.mobile_phone.DTO.ModelDTO;
 import com.kosal.modernPhoneshop.mobile_phone.entities.Model;
+import com.kosal.modernPhoneshop.mobile_phone.service.BrandService;
 
-@Mapper
+@Mapper(componentModel = "spring",uses= {BrandService.class})
 public interface ModeEntityMapper {
 	ModeEntityMapper CREATION=Mappers.getMapper(ModeEntityMapper.class);
-	@Mapping(target = "brand.id",source = "brandId")
+	@Mapping(target = "brand",source = "brandId")
 	Model toModel(ModelDTO modelDTO);
-	@Mapping(target = "brandId",source = "brand.id")
+	
+	//
+	
+	@Mapping(target = "brandId",source = "model.id") 
 	ModelDTO toModelDTO(Model model);
+	 
 
 }
