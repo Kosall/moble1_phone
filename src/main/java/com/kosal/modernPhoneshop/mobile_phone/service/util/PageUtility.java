b99861ddb3d1e0a4e3a49bb87349be35f5844225
Kosal Nim <kosall123nim@gmail.com>
2024-10-09 05:21:19 +0700

    edit some mistakes

--- src/main/java/com/kosal/modernPhoneshop/mobile_phone/service/util/PageUtility.java
+++ src/main/java/com/kosal/modernPhoneshop/mobile_phone/service/util/PageUtility.java
@@ -4,8 +4,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 public interface PageUtility {
-	int DEFAULT_PAGE_NUMBER=2;
-	int DEFAULT_PAGE_LIMIT=1;
+	int DEFAULT_PAGE_NUMBER=1;
+	int DEFAULT_PAGE_LIMIT=2;
 	String PAGE_LIMIT="_limit";
 	String PAGE_NUMBER="_page";
 	static Pageable getPageable(int pageNumber,int pageSize) {
