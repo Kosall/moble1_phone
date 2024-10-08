3471ae161a1cf1edb18b6a0016bf13c92061db80
Kosal Nim <kosall123nim@gmail.com>
2024-10-09 05:16:21 +0700

    No chnages

--- src/main/java/com/kosal/modernPhoneshop/mobile_phone/service/ModelService.java
+++ src/main/java/com/kosal/modernPhoneshop/mobile_phone/service/ModelService.java
@@ -8,6 +8,8 @@ public interface ModelService {
 	Model create(Model model);
 	Model getModelById(Long id);
 	List<Model>getModelByBrandId(Long id);
+	//List<Model>getByBrand(Long id);
+	//List<Model>getByBrandId(Long id);
 	Model updatebyid(Long id,Model forUpdate);
 	
 
