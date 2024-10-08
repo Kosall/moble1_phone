a23cb1365dfca2811c774a0a5e485945f4345ee7
Kosal Nim <kosall123nim@gmail.com>
2024-10-09 05:03:07 +0700

    add getByBrandId method in BrandController.java

--- src/main/java/com/kosal/modernPhoneshop/mobile_phone/controller/BrandController.java
+++ src/main/java/com/kosal/modernPhoneshop/mobile_phone/controller/BrandController.java
@@ -2,12 +2,8 @@ package com.kosal.modernPhoneshop.mobile_phone.controller;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-import org.aspectj.lang.annotation.Before;
-import org.mapstruct.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,14 +20,22 @@ import com.kosal.modernPhoneshop.mobile_phone.DTO.BrandDTO;
 import com.kosal.modernPhoneshop.mobile_phone.entities.Brand;
 import com.kosal.modernPhoneshop.mobile_phone.entities.Model;
 import com.kosal.modernPhoneshop.mobile_phone.mapper.BrandMapper;
+import com.kosal.modernPhoneshop.mobile_phone.mapper.ModeEntityMapper;
 import com.kosal.modernPhoneshop.mobile_phone.page.DTO.PageDTO;
 import com.kosal.modernPhoneshop.mobile_phone.service.BrandService;
+import com.kosal.modernPhoneshop.mobile_phone.service.ModelService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("brands")
+@RequiredArgsConstructor
 public class BrandController {
-	@Autowired
-	private BrandService service;
+	//@Autowired
+	private final BrandService service;
+	//@Autowired
+	private final ModelService modelService;
+	private final ModeEntityMapper entityMapper;
 
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<?> create(@RequestBody BrandDTO brandDTO) {
@@ -81,22 +85,31 @@ public class BrandController {
 	 * 
 	 * }
 	 */
-	
+
+	@GetMapping
+	public ResponseEntity<?> getBrands(@RequestParam Map<String, String> params) {
+
+		Page<Brand> brands = service.getBrands(params);
+		PageDTO dto = new PageDTO(brands);
+
+		return ResponseEntity.ok(dto);
+
+	}
+
 	/*
-	 * @GetMapping public ResponseEntity<?> getBrands(@RequestParam Map<String,
-	 * String> params) {
-	 * 
-	 * Page<Brand> brands = service.getBrands(params); PageDTO dto=new
-	 * PageDTO(brands);
-	 * 
-	 * return ResponseEntity.ok(dto);
-	 * 
-	 * }
-	 */
-	/*
-	 * @GetMapping public ResponseEntity<?>getAll(){ List<BrandDTO> list =
-	 * service.getBrands().stream() .map(br->BrandMapper.INSTANCE.toBrandDTO(br))
-	 * .toList(); return ResponseEntity.ok(list); }
-	 */
-	
+	 * @GetMapping public ResponseEntity<?> getAll() { List<Brand> brands =
+	 * service.getBrands(); // .stream().map(br ->
+	 * BrandMapper.INSTANCE.toBrandDTO(br)).toList(); // List<Brand>brands=new
+	 * ArrayList<>(); // Brand brand = new Brand(); // for(BrandDTO dto:list) { //
+	 * brand = BrandMapper.INSTANCE.toBrand(dto); // brands.add(brand); // } //
+	 * return ResponseEntity.ok(brands); }
+	*/
+	@GetMapping("{id}/models")
+	public ResponseEntity<?>getByBrandId(@PathVariable("id") Long id){
+		List<Model> models = modelService.getModelByBrandId(id);
+		models.stream().map(entityMapper::toModelDTO).toList();
+		return ResponseEntity.ok(models);
+		
+	}
+
 }
