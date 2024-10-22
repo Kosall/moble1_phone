d77e7b555b44e88e718631bce100c545e2a6a9bf
Kosal Nim <kosall123nim@gmail.com>
2024-10-23 04:33:32 +0700

    create strategy = GenerationType.AUTO,generator = mySeqGene and

--- src/main/java/com/kosal/modernPhoneshop/mobile_phone/entities/Model.java
+++ src/main/java/com/kosal/modernPhoneshop/mobile_phone/entities/Model.java
@@ -1,11 +1,13 @@
 package com.kosal.modernPhoneshop.mobile_phone.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -15,8 +17,11 @@ import lombok.Data;
 @Table(name="models")
 public class Model {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO,generator = "mySeqGene")
+	@SequenceGenerator(name="mySeqGene",sequenceName = "models_model_id_seq",initialValue = 100,allocationSize = 1)
+	@Column(name = "model_id",updatable = false,nullable = false)
 	private Long id;
+	@Column(name="model_name")
 	private String name;
 	@ManyToOne
 	@JoinColumn(name="brandId")
