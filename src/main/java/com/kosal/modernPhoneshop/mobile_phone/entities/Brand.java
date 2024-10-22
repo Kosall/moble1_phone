a8fc3ba00f29fb7787517aa688227da5eebb6a6a
Kosal Nim <kosall123nim@gmail.com>
2024-10-23 04:32:13 +0700

    create generator = mySeqGen and name=mySeqGen,sequenceName = brands_brand_id_seq,initialValue = 1,allocationSize = 1 on id

--- src/main/java/com/kosal/modernPhoneshop/mobile_phone/entities/Brand.java
+++ src/main/java/com/kosal/modernPhoneshop/mobile_phone/entities/Brand.java
@@ -1,10 +1,14 @@
 package com.kosal.modernPhoneshop.mobile_phone.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import org.springframework.data.jpa.repository.Query;
 
 import lombok.Data;
 
@@ -12,10 +16,14 @@ import lombok.Data;
 @Entity
 @Table(name="brands")
 public class Brand {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	
+	 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO,generator = "mySeqGen")
+	@SequenceGenerator(name="mySeqGen",sequenceName = "brands_brand_id_seq",initialValue = 1,allocationSize = 1)
+	@Column(name="brand_id",updatable = false,nullable=false)
+	private Long id;
+	@Column(name="brand_name")
 	private String name;
 
 }
